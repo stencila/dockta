ca5892406bf583aa6f123624494b8847b111e1cb
Nokome Bentley <me@noko.me>
2018-10-09 16:06:26 +1300

    Linting

--- src/router.ts
+++ src/router.ts
@@ -12,7 +12,7 @@ function run (method: string) {
       // @ts-ignore
       const node = await compiler[method](req.body)
       res.status(200).json(node)
-    } catch(error) {
+    } catch (error) {
       res.status(500).write(error.stack)
     }
   }
