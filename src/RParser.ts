e93a3894756fba570521d9f9db4c15ef29cd27cb
Nokome Bentley <me@noko.me>
2018-10-15 12:28:08 +1300

    Add system requirements to RParser

--- src/RParser.ts
+++ src/RParser.ts
@@ -2,7 +2,13 @@ import Parser from './Parser'
 import { ComputerLanguage, SoftwarePackage, SoftwareEnvironment, push, Person } from './context'
 
 /**
- * Dockter `Parser` class for R requirements files and source code
+ * Dockter `Parser` class for R requirements files and source code.
+ * 
+ * For each package, we get meta-data from http://crandb.r-pkg.org.
+ * System dependencies for each package are obtained from https://sysreqs.r-hub.io/pkg/xml2.
+ *
+ * Create a `SoftwarePackage` instance to respresent each package
+ * using crosswalks from column "R Package Description" in https://github.com/codemeta/codemeta/blob/master/crosswalk.csv
  */
 export default class RParser extends Parser {
 
@@ -24,22 +30,28 @@ export default class RParser extends Parser {
 
     const environ = new SoftwareEnvironment()
 
-    // Get `date` from DESCRIPTION file
+    // Get `name`
+    const matchName = desc.match(/^Package:\s*(.+)/m)
+    if (matchName) {
+      environ.name = matchName[1]
+    }
+
+    // Get `date`, if no date then use yesterday's date to ensure
+    // packages are available on MRAN
     let date
-    const match1 = desc.match(/Date:\ *(.+)/)
-    if (match1) {
-      date = Date.parse(match1[1])
+    const matchDate = desc.match(/^Date:\s*(.+)/m)
+    if (matchDate) {
+      date = Date.parse(matchDate[1])
       if (isNaN(date)) {
-        throw new Error('Unable to parse date in DESCRIPTION file: ' + match1[1])
+        throw new Error('Unable to parse date in DESCRIPTION file: ' + matchDate[1])
       } else {
         date = new Date(date)
       }
     }
-    // If no date then use yesterday's date
     if (!date) date = new Date(Date.now() - 24 * 3600 * 1000)
-    date = date.toISOString().substring(0,10)
+    environ.datePublished = date.toISOString().substring(0,10)
 
-    // Get package imports from the description file
+    // Get dependencies
     let packages = []
     const start = /^Imports:[ \t]*\n/gm.exec(desc)
     if (start) {
@@ -59,26 +71,24 @@ export default class RParser extends Parser {
       }
     }
 
-    // For each package, query the CRANDB to get a manifest including it's own
-    // dependencies and convert it to a `SoftwareApplication` using the CodeMeta
-    // crosswalk.
-    await Promise.all(packages.map(async name => {
-      const info = await this.fetch(`http://crandb.r-pkg.org/${name}`)
-            
-      // Create a `SoftwarePackage` instance to respresent each package
-      // using crosswalks from column "R Package Description" in https://github.com/codemeta/codemeta/blob/master/crosswalk.csv
-      // and ordered from general, to more specific, classes: 
-      //    Thing > CreativeWork > SoftwareSourceCode > SoftwarePackage
+    // For each dependency, query https://crandb.r-pkg.org to get a manifest including it's own
+    // dependencies and convert it to a `SoftwarePackage`
+    environ.softwareRequirements = await Promise.all(packages.map(async name => {
+      const crandb = await this.fetch(`http://crandb.r-pkg.org/${name}`)
+ 
+      // Create new package instance and populate it's
+      // properties in order of type heirarchy:
+      //   Thing > CreativeWork > SoftwareSourceCode > SoftwarePackage
       const pkg = new SoftwarePackage()
 
       // schema:Thing
-      pkg.description = info.Description
-      pkg.identifiers = [info.Package]
-      pkg.name = info.Title
-      pkg.urls = info.URL.split(',')
+      pkg.description = crandb.Description
+      pkg.name = crandb.Package
+      pkg.urls = crandb.URL.split(',')
       
       // schema:CreativeWork
-      info.Author.split(',\n').map((author: string) => {
+      // pkg.headline = crandb.Title TODO
+      crandb.Author.split(',\n').map((author: string) => {
         const match = author.match(/^([^\[]+?) \[([^\]]+)\]/)
         if (match) {
           const name = match[1]
@@ -91,19 +101,35 @@ export default class RParser extends Parser {
           push(pkg, 'authors', Person.fromText(author))
         }
       })
-      pkg.datePublished = info['Date/Publication']
-      pkg.license = info.License // TODO parse license string into a URL or CreativeWork
+      pkg.datePublished = crandb['Date/Publication']
+      pkg.license = crandb.License // TODO parse license string into a URL or CreativeWork
 
       // schema:SoftwareSourceCode
-      pkg.codeRepository = info.URL.split(',') // TODO only use URLS which point to a repo e.g. github.com
+      pkg.codeRepository = crandb.URL.split(',') // TODO only use URLS which point to a repo e.g. github.com
 
       // stencila:SoftwarePackage
-      // R packages required 
-      for (let [name, version] of Object.entries(info.Imports)) {
+      // Required R packages are added as `softwareRequirements` with
+      // `programmingLanguage` set to R
+      for (let [name, version] of Object.entries(crandb.Imports)) {
         const required = new SoftwarePackage()
+        required.name = name
         required.programmingLanguages = [ComputerLanguage.r]
-        required.identifiers = [name]
         pkg.softwareRequirements.push(required)
+      }
+      // Required system dependencies are obtained from https://sysreqs.r-hub.io and
+      // added as `softwareRequirements` with no `programmingLanguage` 
+      const sysreqs = await this.fetch(`https://sysreqs.r-hub.io/pkg/${name}`)
+      for (let sysreq of sysreqs) {
+        const keys = Object.keys(sysreq)
+        if (keys.length > 1) throw new Error(`Expected on one key for each sysreq but got: ${keys.join(',')}`)
+        const name = keys[0]
+        const debPackage = sysreq[name].platforms['DEB']
+        // The deb package can be null e.g. `curl https://sysreqs.r-hub.io/pkg/lubridate`
+        if (debPackage) {
+          const required = new SoftwarePackage()
+          required.name = debPackage
+          pkg.softwareRequirements.push(required)
+        }
       }
       
       return pkg
--- tests/RParser.test.ts
+++ tests/RParser.test.ts
@@ -2,13 +2,33 @@ import RParser from '../src/RParser'
 import fixture from './fixture'
 import { SoftwareApplication, SoftwareEnvironment } from '../src/context';
 
-test('empty', async () => {
+/**
+ * When applied to an empty folder, parse should return null.
+ */
+test('parse:empty', async () => {
   const parser = new RParser(fixture('empty'))
   expect(await parser.parse()).toBeNull()
 })
 
-test('r-date', async () => {
+/**
+ * When applied to a folder with no R code, parse should return null.
+ */
+test('parse:dockerfile-date', async () => {
+  const parser = new RParser(fixture('empty'))
+  expect(await parser.parse()).toBeNull()
+})
+
+/**
+ * When applied to a folder with a DESCRIPTION file, parse should return
+ * a `SoftwareEnvironment` with `name`, `softwareRequirements` etc
+ * populated correctly.
+ */
+test('parse:r-date', async () => {
   const parser = new RParser(fixture('r-date'))
   const environ = await parser.parse() as SoftwareEnvironment
-  expect(environ.softwareRequirements).toEqual([])
+  expect(environ.name).toEqual('rdate')
+  // expect(environ.version).toEqual('version') TODO version not implemented yet
+  expect(environ.datePublished).toEqual('2018-10-05')
+  expect(environ.softwareRequirements.length).toEqual(1)
+  expect(environ.softwareRequirements[0].name).toEqual('lubridate')
 })
