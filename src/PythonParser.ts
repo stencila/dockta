f1b27298f74b7a563da8d3bc71a7e289464cd37b
Nokome Bentley <me@noko.me>
2018-10-16 09:29:31 +1300

    Remove cruft from PythonParser

--- src/PythonParser.ts
+++ src/PythonParser.ts
@@ -3,19 +3,6 @@ import { SoftwareEnvironment } from './context'
 
 export default class PythonParser extends Parser {
 
-  matchPaths (): Array<string> {
-    return ['requirements.txt', 'cmd.py']
-  }
-
-  aptPackages (sysVersion: number): Array<string> {
-    return ['python3', 'python3-pip']
-  }
-
-  command (sysVersion: number): string {
-    if (this.exists('cmd.py')) return 'python3 cmd.py'
-    else return ''
-  }
-
   async parse (): Promise<SoftwareEnvironment | null> {
     return null
   }
