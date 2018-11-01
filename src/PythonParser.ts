b0fdad5a2162d15419a65479049230276f9e1c59
Ben Shaw <ben@beneboy.co.nz>
2018-11-01 21:34:28 +1300

    Added parsing of PyPI for extra python package metadata

--- src/PythonParser.ts
+++ src/PythonParser.ts
@@ -1,6 +1,12 @@
-import { default as path, dirname, basename } from 'path'
-import { SoftwareEnvironment, SoftwarePackage, SoftwareSourceCode } from '@stencila/schema'
-import fs from 'fs'
+import { dirname, basename } from 'path'
+import {
+  ComputerLanguage,
+  Person,
+  SoftwareApplication,
+  SoftwareEnvironment,
+  SoftwareSourceCode
+} from '@stencila/schema'
+import OperatingSystem from '@stencila/schema/dist/OperatingSystem'
 
 import Parser from './Parser'
 import { default as pythonSystemModules } from './PythonBuiltins'
@@ -52,6 +58,81 @@ function splitStandardRequirementVersion (requirement: string): [string, string 
   return [requirement, null]
 }
 
+function buildClassifierMap (classifiers: Array<string>): Map<string, Array<string>> {
+  const classifierMap = new Map<string, Array<string>>()
+
+  for (let classifier in classifiers) {
+    let doubleColonPosition = classifier.indexOf('::')
+
+    let classifierKey = classifier.substring(0, doubleColonPosition).trim()
+    let classifierValue = classifier.substring(doubleColonPosition + 2).trim()
+
+    if (!classifierMap.has(classifierKey)) {
+      classifierMap.set(classifierKey, [])
+    }
+
+    classifierMap.get(classifierKey)!.push(classifierValue)
+  }
+
+  return classifierMap
+}
+
+/**
+ * Each PyPI "Topic" might contain multiple levels of categorisation separated by "::". E.g.
+ * "Topic :: Category :: Secondary Category :: Tertiary Category". This will split into an array of strings of the same
+ * length as the number of categories, i.e. ["Category", "Secondary Category", "Tertiary Category"]
+ */
+function splitTopic (topics: string): Array<string> {
+  return topics.split('::').map(topic => topic.trim())
+}
+
+/**
+ * Parse an array of PyPI formatted topics into unique lists, returns a tuple of top level and optionally second level
+ * topics. This is because PyPI will repeat top level Topics in sub topics, e.g. the list might contain:
+ * ["Topic :: Game", "Topic :: Game :: Arcade"] hence "Game" is defined twice.
+ */
+function parseTopics (topicsList: Array<string>): [Array<string>, Array<string>] {
+  let primaryTopics: Array<string> = []
+  let secondaryTopics: Array<string> = []
+
+  for (let topics of topicsList) {
+    let splitTopics = splitTopic(topics)
+    if (splitTopics.length) {
+      if (!primaryTopics.includes(splitTopics[0])) primaryTopics.push(splitTopics[0])
+
+      if (splitTopics.length > 1) {
+        if (!secondaryTopics.includes(splitTopics[1])) secondaryTopics.push(splitTopics[1])
+      }
+    }
+  }
+
+  return [primaryTopics, secondaryTopics]
+}
+
+/**
+ * Convert a string containing an operating system name into an array of `OperatingSystem`s. In some instances the
+ * description may map to multiple `OperatingSystems`, e.g. "Unix" => Linux and macOS.
+ */
+function parseOperatingSystem (operatingSystem: string): Array<OperatingSystem> {
+  if (operatingSystem.match(/windows/i)) {
+    return [OperatingSystem.windows]
+  }
+
+  if (operatingSystem.match(/unix/i)) {
+    return [OperatingSystem.linux, OperatingSystem.macos, OperatingSystem.unix]
+  }
+
+  if (operatingSystem.match(/linux/i)) {
+    return [OperatingSystem.linux]
+  }
+
+  if (operatingSystem.match(/macos/i) || operatingSystem.match(/mac os/i)) {
+    return [OperatingSystem.macos]
+  }
+
+  return []
+}
+
 export enum RequirementType {
   Named,
   URL
@@ -89,14 +170,7 @@ export default class PythonParser extends Parser {
 
     for (let rawRequirement of requirements) {
       if (rawRequirement.type === RequirementType.Named) {
-        let standardRequirement = new SoftwarePackage()
-        standardRequirement.name = rawRequirement.value
-        standardRequirement.runtimePlatform = 'Python'
-        if (rawRequirement.version) {
-          standardRequirement.version = rawRequirement.version
-        }
-
-        environ.softwareRequirements.push(standardRequirement)
+        environ.softwareRequirements.push(await this.createApplication(rawRequirement))
       } else if (rawRequirement.type === RequirementType.URL) {
         let sourceRequirement = new SoftwareSourceCode()
         sourceRequirement.runtimePlatform = 'Python'
@@ -105,6 +179,60 @@ export default class PythonParser extends Parser {
     }
 
     return environ
+  }
+
+  private async createApplication (requirement: PythonRequirement): Promise<SoftwareApplication> {
+    const softwareApplication = new SoftwareApplication()
+    softwareApplication.name = requirement.value
+    softwareApplication.runtimePlatform = 'Python'
+    softwareApplication.programmingLanguages = [ComputerLanguage.py]
+
+    if (requirement.version) {
+      softwareApplication.version = requirement.version
+    }
+
+    const pyPiMetadata = await this.fetch(`https://pypi.org/pypi/${softwareApplication.name}/json`)
+
+    if (pyPiMetadata.info) {
+      if (pyPiMetadata.info.author) {
+        softwareApplication.authors.push(Person.fromText(`${pyPiMetadata.info.author} <${pyPiMetadata.info.author_email}>`))
+      }
+
+      if (pyPiMetadata.info.project_url) {
+        softwareApplication.codeRepository = pyPiMetadata.info.project_url
+      }
+
+      if (pyPiMetadata.info.classifiers) {
+        let classifiers = buildClassifierMap(pyPiMetadata.info.classifiers)
+        if (classifiers.has('Topic')) {
+          let [topics, subTopics] = parseTopics(classifiers.get('Topic')!)
+
+          if (topics.length) softwareApplication.applicationCategories = topics
+          if (subTopics.length) softwareApplication.applicationSubCategories = subTopics
+        }
+
+        if (classifiers.has('Operating System')) {
+          const operatingSystems: Array<OperatingSystem> = []
+
+          for (let operatingSystemDescription of classifiers.get('Operating System')!) {
+            for (let operatingSystem of parseOperatingSystem(operatingSystemDescription)) {
+              if (!operatingSystems.includes(operatingSystem)) operatingSystems.push(operatingSystem)
+            }
+          }
+          softwareApplication.operatingSystems = operatingSystems
+        }
+      }
+      if (pyPiMetadata.info.keywords) softwareApplication.keywords = pyPiMetadata.info.keywords
+
+      if (pyPiMetadata.info.license) softwareApplication.license = pyPiMetadata.info.license
+
+      if (pyPiMetadata.info.long_description) {
+        softwareApplication.description = pyPiMetadata.info.long_description
+      } else if (pyPiMetadata.info.description) {
+        softwareApplication.description = pyPiMetadata.info.description
+      }
+    }
+    return softwareApplication
   }
 
   async parseRequirementsFile (path: string): Promise<Array<PythonRequirement>> {
