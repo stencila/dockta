c5e4d96d197f111ec11ac6ee3fb7a43399957ff9
Nokome Bentley <nokome@stenci.la>
2019-05-14 14:18:38 +1200

    chore: allow version to be specified in install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,28 @@ if [[ "$OS" == "Linux" || "$OS" == "Darwin" ]]; then
     case "$OS" in
         'Linux')
             PLATFORM="linux-x64"
-            VERSION=$(curl --silent "https://api.github.com/repos/stencila/dockter/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+            if [ -z "$1" ]; then
+                VERSION=$(curl --silent "https://api.github.com/repos/stencila/dockter/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+            else
+                VERSION=$1
+            fi
             INSTALL_PATH="$HOME/.local/bin/"
             ;;
         'Darwin')
             PLATFORM="macos-x64"
-            VERSION=$(curl --silent "https://api.github.com/repos/stencila/dockter/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+            if [ -z "$1" ]; then
+                VERSION=$(curl --silent "https://api.github.com/repos/stencila/dockter/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+            else
+                VERSION=$1
+            fi
             INSTALL_PATH="/usr/local/bin/"
             ;;
     esac
-    curl -Lo /tmp/dockter-linux-x64.tar.gz https://github.com/stencila/dockter/releases/download/$VERSION/dockter-$PLATFORM.tar.gz
-    tar xvf /tmp/dockter-linux-x64.tar.gz
+    curl -Lo /tmp/dockter.tar.gz https://github.com/stencila/dockter/releases/download/$VERSION/dockter-$PLATFORM.tar.gz
+    tar xvf /tmp/dockter.tar.gz
     mkdir -p $INSTALL_PATH
     mv -f dockter $INSTALL_PATH
-    rm -f /tmp/dockter-linux-x64.tar.gz
+    rm -f /tmp/dockter.tar.gz
 else
     echo "Sorry, I don't know how to install on this OS, please see https://github.com/stencila/dockter#install"
 fi
