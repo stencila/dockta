003f480ed3da6c0e031533e094b82cd7b184619a
Nokome Bentley <nokome@stenci.la>
2019-11-27 12:16:22 +1300

    fix(Dependencies): Update dependencies

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  coveragePathIgnorePatterns: [
-    'tests/MockUrlFetcher.ts',
-    'tests/test-functions.ts'
-  ]
-}
